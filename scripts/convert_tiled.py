70d625c884249385bc7cd6fd6a5b6d0c15b46481
Chris Handy <maximinus@gmail.com>
2017-11-13 22:17:02 +0000

    Dump of folder layout so far@

---
+++ scripts/convert_tiled.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# load and convert tiled maps
+# this is a command line utility, options:
+
+# convert_tiled.py --render new_file.png
+# or
+# convert_tiled.py -r new_file.png
+
+
+if __name__ == '__main__':
+    print('Hello, World')
