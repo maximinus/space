a48f3d5cc00c0009d17a22d8ff277b59bbfb3706
Chris Smith <chris.smith@actual-experience.com>
2017-11-14 16:31:56 +0000

    Script loads json maps

--- scripts/convert_tiled.py
+++ scripts/convert_tiled.py
@@ -1,18 +1,51 @@
 #!/usr/bin/env python3
 
-import sys
 import argparse
+import json
+import os
+import pygame
+import sys
+
+
+TILE_IMAGES = '../terrain/terrain.png'
+
 
 # load and convert tiled maps
-# this is a command line utility, options:
-
-# convert_tiled.py --render new_file.png
-# or
-# convert_tiled.py -r new_file.png
+# this is a command line utility
 
 
-def renderMap():
-    print('Rendering')
+def loadMapData(map_file):
+    # check 1: the file exists, 2: We can load it, 3: It parses fine
+    if not os.path.isfile(map_file):
+        print('{0} is not a file'.format(map_file))
+        sys.exit(False)
+    try:
+        map_handle = open(map_file, 'r')
+    except IOError:
+        print('Could not open file {0}'.format(map_file))
+        sys.exit(False)
+    # load the data and parse it
+    try:
+        map_data = json.load(map_handle)
+    except Exception as e:
+        print('Could not load {0}: {1}'.format(map_file, e))
+        sys.exit(False)
+    return map_data
+
+
+def renderMap(map_file):
+    map_data = loadMapData(map_file)
+    # now we need to load associated bitmap to render
+    tile_images = pygame.image.load(TILE_IMAGES)
+    # find the individual layers in the json data
+    if 'layers' not in map_data:
+        print('Error: There are no layers to render in {0}!'.format(map_file))
+        sys.exit(False)
+    for layer in map_data['layers']:
+        print(layer['name'])
+    # render each to a surface
+    # merge the surfaces
+    # save this file
 
 
 def showMapInfo():
@@ -36,7 +69,7 @@ if __name__ == '__main__':
 
     if options.render:
         # render the given map
-        renderMap()
+        renderMap(options.render)
     elif options.info:
         showMapInfo()
 
