265c5384a3b2d50aa697ce5f9c5fbcc35c6963c9
Chris Handy <maximinus@gmail.com>
2017-11-14 22:30:21 +0000

    Renderer reads xml and json map data files

--- scripts/convert_tiled.py
+++ scripts/convert_tiled.py
@@ -5,13 +5,25 @@ import json
 import os
 import pygame
 import sys
+import xml.etree.ElementTree as ET
 
 
 TILE_IMAGES = '../terrain/terrain.png'
+XML_TILES = '../terrain/Terrain.tsx'
 
 
 # load and convert tiled maps
 # this is a command line utility
+
+def loadXMLTiles():
+    # parse the tile xml and return an index:rect dict
+    tiles = {}
+    tree = ET.parse(XML_TILES)
+    root = tree.getroot()
+    # get every tile
+    for tile in root.iter('tile'):
+        print(tile.attrib['id'])
+        print(tile.attrib['terrain'])
 
 
 def loadMapData(map_file):
@@ -35,13 +47,12 @@ def loadMapData(map_file):
 
 def getMapLayers(map_data):
     if 'layers' not in map_data:
-        print('Error: There are no layers to render in {0}!'.format(map_file))
+        print('Error: There are no layers to render!')
         sys.exit(False)
     layers = map_data['layers']
     # make sure they all have data
-    required = ['data', 'width', 'height', 'name']
     for i in layers:
-        for j in layers:
+        for j in ['data', 'width', 'height', 'name']:
             if j not in i:
                 print('Error: Missing layer info {0} in layer'.format(j))
                 sys.exit(False)
@@ -69,31 +80,44 @@ def getSize(data):
     return ItemSize(width, height)
 
 
+def saveRenderedLayer(final_surface, map_file):
+    # change the file extension to png
+    new_filename = '{0}{1}'.format(os.path.splitext(map_file)[0], '.png')
+    # save the rendered surface
+    pygame.image.save(final_surface, new_filename)
+
+
 def renderMap(map_file):
     map_data = loadMapData(map_file)
     # now we need to load associated bitmap to render
-    #tile_images = pygame.image.load(TILE_IMAGES)
+    tile_images = pygame.image.load(TILE_IMAGES)
     # find the individual layers in the json data
 
     layers = getMapLayers(map_data)
-    map_size = getMapSize(map_data)
+    map_size = getSize(map_data)
     render_layers = []
     for layer in layers:
         # render each to a surface
         try:
             length = layer['width'] * layer['height']
             data = layer['data']
-            name = layer['name']
         except KeyError as e:
             print('Map data is missing elements: {0}'.format(e))
             sys.exit(0)
-        render_surface = pygame.Surface((xsize, ysize))
+        render_surface = pygame.Surface((map_size.width, map_size.height))
         if len(data) != length:
             print('Error: data length different from map size in layer {0}.'.format('name'))
             sys.exit(0)
         for tile_value in data:
             # find this tile
             # blit it with full alpha
+            pass
+        render_layers.append(render_surface)
+    # render layers 1, 2 etc onto layer 0
+    for layer in render_layers[1:]:
+        render_layers[0].blit(layer, (0, 0))
+    # save the rendered layer
+    saveRenderedLayer(render_layers[0], map_file)
 
 
 def showMapInfo():
@@ -102,12 +126,10 @@ def showMapInfo():
 
 def setParseOptions():
     parser = argparse.ArgumentParser(description='Converter for Tiled json maps')
-
     render_help = 'Render the given map to a single image'
     parser.add_argument('-r', '--render', metavar='map', help=render_help)
     info_help = 'Show map statistics'
     parser.add_argument('-i', '--info', metavar='map', help=info_help)
-
     return parser
 
 
@@ -124,3 +146,5 @@ if __name__ == '__main__':
     if len(sys.argv) == 1:
         # no arguments were passes
         parser.print_help()
+
+    loadXMLTiles()
