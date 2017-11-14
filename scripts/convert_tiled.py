560e7757e57aae2b9e925add390e03b747d3070f
Chris Smith <chris.smith@actual-experience.com>
2017-11-14 11:03:43 +0000

    Map renderer accepts arguments

--- scripts/convert_tiled.py
+++ scripts/convert_tiled.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import sys
+import argparse
+
 # load and convert tiled maps
 # this is a command line utility, options:
 
@@ -8,5 +11,35 @@
 # convert_tiled.py -r new_file.png
 
 
+def renderMap():
+    print('Rendering')
+
+
+def showMapInfo():
+    print('Map info')
+
+
+def parseOptions():
+    parser = argparse.ArgumentParser(description='Converter for Tiled json maps')
+
+    render_help = 'Render the given map to a single image'
+    parser.add_argument('-r', '--render', metavar='map', help=render_help)
+    info_help = 'Show map statistics'
+    parser.add_argument('-i', '--info', metavar='map', help=info_help)
+
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    print('Hello, World')
+    options = parseOptions()
+
+    if options.render:
+        # Show details for a specific interface
+        renderMap()
+    elif options.info:
+        showMapInfo()
+
+    if len(sys.argv) == 1:
+        # no arguments were passes
+        parser.print_help()
+        sys.exit(False)
